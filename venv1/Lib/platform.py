13506917513e734e31ebed8a4c52d1210bb37fed
Shijin MohananSheela <getshijin@gmail.com>
2025-01-16 13:21:07 +0100

    '16/01/2025_on_version_1'

--- venv1/Lib/platform.py
+++ venv1/Lib/platform.py
@@ -966,7 +966,7 @@ def processor():
 ### Various APIs for extracting information from sys.version
 
 _sys_version_parser = re.compile(
-    r'([\w.+]+)\s*(?:\ \|\ packaged\ by\ Anaconda,\ Inc\.\ \|)?\s*'
+    r'([\w.+]+)\s*(?:\ \|\ packaged\ by\ conda\-forge\ \|)?\s*'
     r'\(#?([^,]+)'  # "(#buildno"
     r'(?:,\s*([\w ]*)'  # ", builddate"
     r'(?:,\s*([\w :]*))?)?\)\s*'  # ", buildtime)<space>"
